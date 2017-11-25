0e2457ff0922a94d61a5ae4ce0fb772a0f4ee3b6
yiboduan <yibo.duan@gmail.com>
2017-11-25 11:19:39 -0600

    wave spawner

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour {
+
+	public Transform runnerPrefab;
+	public Transform spawnPoint;
+
+	public static float countdown = 2f;
+	private float secondsUntilSpawn = countdown;
+	public float secondsBetweenWaves = 5f;
+
+	public float spawnDelaySeconds = 0.5f;
+
+	public float enemyCount = 5f;
+
+	void Update() {
+		if (secondsUntilSpawn <= 0f) {
+			StartCoroutine (SpawnWave ());
+			secondsUntilSpawn = secondsBetweenWaves;
+		}
+		secondsUntilSpawn -= Time.deltaTime;
+	}
+
+	IEnumerator SpawnWave() {
+		for (int i = 0; i < enemyCount; i++) {
+			Instantiate (runnerPrefab, spawnPoint.position, spawnPoint.rotation);
+			yield return new WaitForSeconds (spawnDelaySeconds);
+		}
+	}
+		
+}
