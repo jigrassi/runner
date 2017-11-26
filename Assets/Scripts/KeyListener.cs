e3d007c6aae123767921ffa92cfff2cc96110436
Yibo Duan <yibo.duan@gmail.com>
2017-11-26 13:24:57 -0600

    broken toggle script

---
+++ Assets/Scripts/KeyListener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyListener : MonoBehaviour {
+	void OnGUI() {
+		Event e = Event.current;
+		switch (e.keyCode) {
+			case KeyCode.S:
+				Debug.Log("S key pressed");
+				
+				if (ToggleWaveSpawner.spawn) {
+					ToggleWaveSpawner.spawn = !ToggleWaveSpawner.spawn;
+				}
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/ToggleWaveSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleWaveSpawner : MonoBehaviour {
+
+	public Transform runnerPrefab;
+	public Transform spawnPoint;
+
+	// spawn attributes
+	public float secondsBetweenWaves = 5f;
+	public float spawnDelaySeconds = 0.5f;
+	public float enemyCount = 5f;
+
+	// toggleable spawn flag
+	public static bool spawn = false;
+
+	private float secondsUntilSpawn = 0;
+
+	void Update() {
+		if (!spawn)
+			return;
+		
+		if (secondsUntilSpawn <= 0) {
+			StartCoroutine (SpawnWave ());
+			secondsUntilSpawn = secondsBetweenWaves;
+		}
+		secondsUntilSpawn -= Time.deltaTime;
+	}
+
+	IEnumerator SpawnWave() {
+		for (int i = 0; i < enemyCount; i++) {
+			Instantiate (runnerPrefab, spawnPoint.position, spawnPoint.rotation);
+			yield return new WaitForSeconds (spawnDelaySeconds);
+		}
+	}
+
+}
