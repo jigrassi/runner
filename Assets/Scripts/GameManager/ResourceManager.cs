171a26d6721bab9df8b67e7132faef1ad425e6a7
yiboduan <yibo.duan@gmail.com>
2018-01-19 01:17:11 -0600

    its working!

---
+++ Assets/Scripts/GameManager/ResourceManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour {
+
+	public static ResourceManager instance;
+
+	private Dictionary<string, Sprite> sprite_cache = new Dictionary<string, Sprite>();
+
+	void Awake () {
+		if (instance != null) {
+			Debug.LogError("More than one ResourceManager in scene");
+			return;
+		}
+		instance = this;
+	}
+
+	private const string sprite_base_path = "Sprites/";
+
+	public Sprite LoadSprite(string name) {
+		if (!sprite_cache.ContainsKey(name)) {
+			sprite_cache[name] = Resources.Load<Sprite>(sprite_base_path + name);
+			if (sprite_cache == null) {
+				Debug.LogErrorFormat("Could not find sprite: {0}!", name);
+			}
+		}
+
+		return sprite_cache[name];
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public abstract class Tile {
+	public abstract bool buildable { get; }
+
+	public abstract bool traversible { get; }
+
+	public abstract Sprite sprite { get; }
+}
